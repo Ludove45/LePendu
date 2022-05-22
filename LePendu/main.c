f8b90c9be09541f68b841dc223732b6725c985b3
Ludove45 <ludove45@gmail.com>
2022-05-22 22:48:14 +0200

    first working letter tester

--- LePendu/main.c
+++ LePendu/main.c
@@ -7,9 +7,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool letter_in_word(char* word,char letter)
+{
+    int length = strlen(word);
+    for(int i=0;i<length;i++)
+    {
+        if(word[i]== letter)
+            return true;
+    }
+    return false;
+}
 
 int main()
 {
+    char* word = "pendu";
+    int try = 0;
+    char userLetter = 0;
+    
+    do
+    {
+        // saisi utilisateur
+        printf("votre lettre :");
+        scanf(" %c",&userLetter);
+        
+        if (letter_in_word(word,userLetter))
+            printf("ok");
+        
+    }
+    while (try>10);
+    
     
     printf("\n");
     return 0;
